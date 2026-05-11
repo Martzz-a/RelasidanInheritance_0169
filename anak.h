98f7b0d92755743582829325dd12cd7b28cd4b06
Martzz-a <maritzafidelyah@gmail.com>
2026-05-11 15:47:51 +0700

    Menambahkan Header Guard

--- anak.h
+++ anak.h
@@ -1,18 +1,4 @@
 #ifndef ANAK_H
 #define ANAK_H
 
-#include <iostream>
-using namespace std;
-
-class anak {
-public:
-    std::string nama;
-    anak(string pNama) :nama(pNama) {
-        cout << "Anak \"" << nama << "\" ada\n";
-    }
-
-    ~anak(){
-        cout << "Anak \"" << nama << "\" tidak ada\n";
-    }
-};
 #endif
