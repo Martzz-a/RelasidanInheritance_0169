7dd0c1afd7a7c12cc1cb70206146da3a197f9e82
Martzz-a <maritzafidelyah@gmail.com>
2026-05-11 16:10:31 +0700

    Menambahkan Header Guard

--- jantung.h
+++ jantung.h
@@ -1,16 +1,3 @@
 #ifndef JANTUNG_H
 #define JANTUNG_H
-
-#include <iostream>
-using namespace std;
-
-class jantung {
-public:
-    jantung() {
-        cout << "jantung dihidupkan\n";
-    }
-    ~jantung() {
-        cout << "jantung dimatikan\n";
-    }
-};
 #endif
