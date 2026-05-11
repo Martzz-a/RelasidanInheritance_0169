dc822e5ebe99b2c78080393cec8e561d51b42ddd
Martzz-a <maritzafidelyah@gmail.com>
2026-05-11 15:48:36 +0700

    Menambahkan Class Anak

--- anak.h
+++ anak.h
@@ -3,4 +3,16 @@
 
 #include <iostream>
 using namespace std;
+
+class anak {
+public:
+    std::string nama;
+    anak(string pNama) :nama(pNama) {
+        cout << "Anak \"" << nama << "\" ada\n";
+    }
+
+    ~anak(){
+        cout << "Anak \"" << nama << "\" tidak ada\n";
+    }
+};
 #endif
